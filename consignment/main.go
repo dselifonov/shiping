be5521b1a062e74f5f3f6c7d7fca5b3d3d656aa7
DSelifonov <dselifonov@ozon.ru>
2020-09-30 13:11:55 +0300

    shipping service

--- consignment/main.go
+++ consignment/main.go
@@ -1,19 +1,63 @@
 package main
 
 import (
+	"context"
 	pb "github.com/dselifonov/shiping/consignment/proto"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/reflection"
+	"log"
+	"net"
 	"sync"
 )
 
 const port = ":50051"
 
+// Repo Code
 type repository interface {
-	Create(*pb.Consignment) (*pb.Consignment, error)
+	Create(consignment *pb.Consignment) (*pb.Consignment, error)
 }
 
 type Repository struct {
 	mu           sync.Mutex
 	consignments []*pb.Consignment
+}
+
+func (repo *Repository) Create(consignment *pb.Consignment) (*pb.Consignment, error) {
+	repo.mu.Lock()
+	repo.consignments = append(repo.consignments, consignment)
+	repo.mu.Unlock()
+	return consignment, nil
+}
+
+// Service Code
+type service struct {
+	repo repository
+}
+
+func (s *service) CreateConsignment(ctx context.Context, req *pb.Consignment) (*pb.Response, error) {
+	consignment, err := s.repo.Create(req)
+	if err != nil {
+		return nil, err
+	}
+	return &pb.Response{Created: true, Consignment: consignment}, nil
+}
+
+// Main func
+func main() {
+	repo := &Repository{}
+
+	lis, err := net.Listen("tcp", port)
+	if err != nil {
+		log.Fatalf("failed to listen to: %v", err)
+	}
+
+	s := grpc.NewServer()
+
+	pb.RegisterShippingServiceServer(s, &service{repo})
+	reflection.Register(s)
+
+	log.Println("running on port: ", port)
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
 }
